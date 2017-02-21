b6eb460098729f911c80f2ba99df3cbe433ce229
AsimHabib <asimhabib2@gmail.com>
2017-02-21 09:26:14 -0500

    first commit

---
+++ js/custom.js
@@ -0,0 +1,48 @@
+/* Nav Bar
+===============================*/
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 50) {
+    $('nav').addClass('shrink');
+    $('.navbar-brand > img').addClass('logo-shrink');
+    $('.navbar-default .navbar-nav > li > a').addClass('xyz');
+  } else {
+    $('nav').removeClass('shrink');
+    $('.navbar-brand > img').removeClass('logo-shrink');
+    $('.navbar-default .navbar-nav > li > a').removeClass('xyz');
+  }
+});
+
+/* Scroll To Top
+===============================*/
+var $ = jQuery.noConflict();
+jQuery(document).ready(function( $ ){
+    scrollToTop.init( );
+});
+
+var scrollToTop =
+{
+    /**
+     * When the user has scrolled more than 100 pixels then we display the scroll to top button using the fadeIn function
+     * If the scroll position is less than 100 then hide the scroll up button
+     *
+     * On the click event of the scroll to top button scroll the window to the top
+     */
+    init: function(  ){
+
+        //Check to see if the window is top if not then display button
+        $(window).scroll(function(){
+            if ($(this).scrollTop() > 1200) {
+                $('.scrollToTop').fadeIn();
+            } else {
+                $('.scrollToTop').fadeOut();
+            }
+        });
+
+        // Click event to scroll to top
+        $('.scrollToTop').click(function(){
+            $('html, body').animate({scrollTop : 0},800);
+            return false;
+        });
+    }
+};
+
